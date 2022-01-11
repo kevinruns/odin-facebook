8843e3e9b409164a7922f419eb6996a68f4f2ab0
Kevin Lenehan <klenehan123@gmail.com>
2022-01-11 20:50:35 +0000

    user page shows posts

--- app/controllers/notifications_controller.rb
+++ app/controllers/notifications_controller.rb
@@ -12,6 +12,7 @@ class NotificationsController < ApplicationController
     @notifications = Notification.where(recipient: current_user).unread
     @notifications.update_all(read_at: Time.zone.now)
     render json: {success: true}
+
   end
 
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,6 +3,7 @@ class UsersController < ApplicationController
   #users_controller.rb
   def show
     @user = User.find(params[:id])
+    @user_posts = Post.all.order('created_at DESC').where(author_id: current_user)
   end
 
   def index
